3ed7668fc5958bc8ff05818711b187cb5ba76249
Deepak Bisht <beast@Deepaks-MacBook-Air-2.local>
2024-08-07 19:16:59 +0100

    add shutil

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, jsonify, render_template
 from flask_cors import CORS
-import os
+import os, shutil
 from langchain_community.document_loaders import PyPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_openai import OpenAIEmbeddings, ChatOpenAI
